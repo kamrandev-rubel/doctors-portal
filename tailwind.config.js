4be50ff25329a1a11b1ba9e8ed70e74a39221934
kamrandev-rubel <kamranuddin.rubel1@gmail.com>
2022-05-10 13:43:13 +0300

    added navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,9 @@ module.exports = {
     themes: [
       {
         mytheme: {
-          primary: "#a991f7",
-          secondary: "#f6d860",
-          accent: "#37cdbe",
-          neutral: "#3d4451",
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
           "base-100": "#ffffff",
         },
       },
