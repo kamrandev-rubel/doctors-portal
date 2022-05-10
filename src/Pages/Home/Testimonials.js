ab48a1b13f75a50a80ffb3aae4d5862720951381
kamrandev-rubel <kamranuddin.rubel1@gmail.com>
2022-05-11 00:27:25 +0300

    added testimonials

--- src/Pages/Home/Testimonials.js
+++ src/Pages/Home/Testimonials.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import quote from '../../assets/icons/quote.svg'
 
 const Testimonials = () => {
     return (
-        <section>
+        <section className='px-14'>
             <div>
-
+                <div className='flex justify-between items-center'>
+                    <div>
+                        <h3 className='text-primary font-bold text-xl mb-3'>Testimonials</h3>
+                        <p className='text-secondery text-4xl'>What Our Patients Says</p>
+                    </div>
+                    <div>
+                        <img src={quote} className='w-24 md:w-48' alt="" />
+                    </div>
+                </div>
+                <div></div>
             </div>
         </section>
     );
