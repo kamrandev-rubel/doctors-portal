f31b96f280e07a0bfd0c62a47d89403b73a2429e
kamrandev-rubel <kamranuddin.rubel1@gmail.com>
2022-05-11 20:25:02 +0300

    get available appointment data

--- src/Pages/Appointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment.js
@@ -1,10 +1,21 @@
 import { format } from 'date-fns';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const AvailableAppointment = ({ date }) => {
+    const [services, setServices] = useState([]);
+    useEffect(() => {
+        axios.get('services.json')
+            .then((response => {
+                setServices(response.data)
+            }))
+    }, [])
     return (
         <div>
             <h4 className='text-secondary text-xl text-center'> Available Appointments on {format(date, 'PP')}</h4>
+            <div>
+
+            </div>
         </div>
     );
 };
