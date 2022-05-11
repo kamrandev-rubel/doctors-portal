eb83fb339b0d52f1e1d69aa4afb653f575595fea
kamrandev-rubel <kamranuddin.rubel1@gmail.com>
2022-05-11 05:46:13 +0300

    added footer menu

--- src/Pages/Shared/Footer.js
+++ src/Pages/Shared/Footer.js
@@ -7,34 +7,31 @@ const Footer = () => {
         <footer
             style={{
                 backgroundImage: `url(${footer})`,
-                backgroundSize: '100%',
+                backgroundSize: 'cover',
             }}>
             <div className="footer p-10 text-base-content py-24">
                 <div>
                     <span className="footer-title">Services</span>
-                    <Link to='/' className="link link-hover">Branding</Link>
-                    <Link to='/' className="link link-hover">Design</Link>
-                    <Link to='/' className="link link-hover">Marketing</Link>
-                    <Link to='/' className="link link-hover">Advertisement</Link>
+                    <Link to='/' className="link link-hover text-accent">Emergency Checkup</Link>
+                    <Link to='/' className="link link-hover text-accent">Monthly Checkup</Link>
+                    <Link to='/' className="link link-hover text-accent">Weekly Checkup</Link>
+                    <Link to='/' className="link link-hover text-accent">Deep Checkup</Link>
                 </div>
                 <div>
-                    <span className="footer-title">Company</span>
-                    <Link to='/' className="link link-hover">About us</Link>
-                    <Link to='/' className="link link-hover">Contact</Link>
-                    <Link to='/' className="link link-hover">Jobs</Link>
-                    <Link to='/' className="link link-hover">Press kit</Link>
+                    <span className="footer-title">ORAL HEALTH</span>
+                    <Link to='/' className="link link-hover text-accent">Fluoride Treatment</Link>
+                    <Link to='/' className="link link-hover text-accent">Cavity Filling</Link>
+                    <Link to='/' className="link link-hover text-accent">Teath Whitening</Link>
                 </div>
                 <div>
-                    <span className="footer-title">Legal</span>
-                    <Link to='/' className="link link-hover">Terms of use</Link>
-                    <Link to='/' className="link link-hover">Privacy policy</Link>
-                    <Link to='/' className="link link-hover">Cookie policy</Link>
+                    <span className="footer-title">OUR ADDRESS</span>
+                    <Link to='/' className="link link-hover text-accent">New York - 101010 Hudson</Link>
                 </div>
                 <div>
                     <span className="footer-title">Newsletter</span>
                     <div className="form-control w-80">
                         <label className="label">
-                            <span className="label-text">Enter your email address</span>
+                            <span className="label-text text-accent">Enter your email address</span>
                         </label>
                         <div className="relative">
                             <input type="text" placeholder="username@site.com" className="input input-bordered w-full pr-16" />
