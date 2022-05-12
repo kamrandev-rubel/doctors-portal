581752c263dcb353d0d0cd4d908076de8014747d
kamrandev-rubel <kamranuddin.rubel1@gmail.com>
2022-05-13 02:49:00 +0300

    add form error handle

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,20 +1,49 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { BsGoogle } from 'react-icons/bs';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const { register, formState: { errors }, handleSubmit } = useForm();
+
+    const onSubmit = data => {
+        console.log(data)
+    };
+
     return (
         <div>
             <div className=" flex justify-center items-center h-screen">
                 <div className="card w-full max-w-[385px] shadow-2xl bg-base-100">
                     <div className="card-body">
                         <h2 className='text-center text-xl font-medium'>Login</h2>
-                        <form>
+                        <form onSubmit={handleSubmit(onSubmit)}>
                             <div className="form-control">
                                 <label htmlFor='email' className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" name='email' id='email' placeholder="Email" className="input input-bordered" />
+                                <input
+                                    type="email"
+                                    id='email'
+                                    placeholder="Email"
+                                    className="input input-bordered"
+                                    {...register("email", {
+                                        required: {
+                                            value: true,
+                                            // message: 'Email address is Required' // JS only: <p>error message</p> TS only support string
+                                        },
+                                        pattern: {
+                                            value: /^[\w._-]+[+]?[\w._-]+@[\w.-]+\.[a-zA-Z]{2,6}$/,
+                                            // message: 'Please enter a valid Email' // JS only: <p>error message</p> TS only support string
+                                        }
+                                    })}
+                                />
+                                <label class="">
+                                    {errors.email?.type === 'required' && <span className='text-xs text-red-600'>Email address is required</span>}
+                                    {errors.email?.type === 'pattern' && <span className='text-xs text-red-600'>Please enter a valid Email</span>}
+                                </label>
                             </div>
                             <div className="form-control">
                                 <label htmlFor='password' className="label">
@@ -37,6 +66,7 @@ const Login = () => {
                             </p>
                             <div className="divider">OR</div>
                             <button
+                                onClick={() => signInWithGoogle()}
                                 className="btn btn-outline btn-accent w-full">
                                 <BsGoogle className='text-2xl mr-2' />
                                 CONTINUE WITH GOOGLE
